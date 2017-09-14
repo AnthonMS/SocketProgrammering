87f99b967ba204bdc4ce6ef9a79947c463ca85b7
jibauch94 <jib090994@gmail.com>
2017-09-14 11:39:04 +0200

    client done

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+
+import static java.lang.System.out;
 
 public class Client {
     public static void main(String[]args){
@@ -15,7 +19,30 @@ public class Client {
                 OutputStream output = s.getOutputStream();
 
                 Scanner in = new Scanner(input);
+
+                PrintWriter out = new PrintWriter(output, true);
+
+                String welcome = in.nextLine();
+                out.println(welcome);
+
+                out.println("Første besked");
+
+                System.out.println(in.nextLine());
+
+                out.println("Anden besked");
+
+                System.out.println(in.nextLine());
+
+                out.println("Tredje besked");
+
+                System.out.println(in.nextLine());
+
+                s.close();
+                System.out.println("Forbindelsen lukket.");
             }
+        }
+        catch (IOException ex){
+            //we ignore this
         }
     }
 }
